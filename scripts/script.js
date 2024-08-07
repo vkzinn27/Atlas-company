9ab04e136d19e3793c6d7d78545192cbea160e28
Estagio TIC 1 - Kassen <estagio.tic1@costao.com.br>
2024-08-07 11:17:05 -0300

    commit carrossel

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
-console.log("Welcome to Atlas Company");  
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
