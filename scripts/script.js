67d2ab926a4d666e4f3f20bfd0809f6ea17ba3c5
vkzinn27 <kassen.sammour@gmail.com>
2024-09-26 11:47:28 -0300

    modais e js para adicionar no carrinho

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,52 @@ const swiper = new Swiper('.swiper-container', {
         disableOnInteraction: false,
     },
 });
+
+document.querySelectorAll('.product-item').forEach(item => {
+    item.addEventListener('click', function() {
+        const title = this.getAttribute('data-title');
+        const price = this.getAttribute('data-price');
+        const image = this.getAttribute('data-image');
+
+        Swal.fire({
+            title: title,
+            text: `Preço: ${price}`,
+            imageUrl: image,
+            imageWidth: 400,
+            imageHeight: 200,
+            imageAlt: title,
+            confirmButtonText: 'Adicionar ao carrinho',
+            customClass: {
+                confirmButton: 'my-confirm-button',
+              }
+        });
+    });
+});
+
+let cartCount = 0;
+
+document.querySelectorAll('.product-item').forEach(item => {
+    item.addEventListener('click', function() {
+        const title = this.getAttribute('data-title');
+        const price = this.getAttribute('data-price');
+        const image = this.getAttribute('data-image');
+
+        Swal.fire({
+            title: title,
+            text: `Preço: ${price}`,
+            imageUrl: image,
+            imageWidth: 400,
+            imageHeight: 200,
+            imageAlt: title,
+            confirmButtonText: 'Adicionar ao carrinho',
+            customClass: {
+                confirmButton: 'my-confirm-button',
+            }
+        }).then((result) => {
+            if (result.isConfirmed) {
+                cartCount++;
+                document.querySelector('.cart-badge').textContent = cartCount;
+            }
+        });
+    });
+});
